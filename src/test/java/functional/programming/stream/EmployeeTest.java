9ac2e21b67fc1c2a640a7861221fbd0b016fd5fd
Brajesh Kumar <kbrajesh176@gmail.com>
2020-07-19 00:43:16 +0100

    Added find first

--- src/test/java/functional/programming/stream/EmployeeTest.java
+++ src/test/java/functional/programming/stream/EmployeeTest.java
@@ -5,13 +5,15 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 public class EmployeeTest {
@@ -46,14 +48,29 @@ public class EmployeeTest {
     public void testFilter() {
         final Integer[] empIds = {1, 2, 3, 4};
         final List<Employee> testEmployees = Arrays.asList(empIds).stream()
-                .map(id -> new Employee(id, "test" + id, 10000.0 * id))
-                .filter(employee -> employee.getSalary() > 20000.0)
+                .map(id -> new Employee(id, "test" + id, Double.valueOf(10000 * id)))
+                .filter(employee ->  employee.getSalary() > Double.valueOf(20000).doubleValue())
                 .collect(Collectors.toList());
 
         assertThat(testEmployees, hasSize(equalTo(2)));
         assertThat(testEmployees, contains(
-                hasProperty("salary", greaterThan(20000.0)),
-                hasProperty("salary", greaterThan(20000.0))
+                hasProperty("salary", greaterThan(Double.valueOf(20000))),
+                hasProperty("salary", greaterThan(Double.valueOf(20000)))
         ));
+    }
+
+
+    @Test
+    public void testFindFirst() {
+        final Integer[] empIds = {1, 2, 3, 4};
+        final Employee testEmployee = Arrays.asList(empIds).stream()
+                .map(id -> new Employee(id, "test" + id, Double.valueOf(10000 * id)))
+                .filter(employee -> employee.getSalary() >= Double.valueOf(20000))
+                .findFirst()
+                .orElse(null);
+
+        assertNotNull(testEmployee);
+        assertEquals(testEmployee.getSalary(), Double.valueOf(20000));
+        //assertEquals(testEmployee.getId(), 2);
     }
 }
