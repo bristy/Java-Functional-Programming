c671e891446c00d89df2020f42e5539fff8068e6
Brajesh Kumar <kbrajesh176@gmail.com>
2020-07-18 23:59:34 +0100

    fix imports

--- src/test/java/functional/programming/stream/EmployeeTest.java
+++ src/test/java/functional/programming/stream/EmployeeTest.java
@@ -1,13 +1,11 @@
 package functional.programming.stream;
 
-import static org.hamcrest.Matchers.contains;
-
-import functional.programming.stream.Employee;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.junit.Assert.assertThat;
